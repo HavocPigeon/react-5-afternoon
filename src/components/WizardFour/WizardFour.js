fae6e7f6d5a3aef1644531da10ab0251e28e2543
HavocPigeon <speeas@gmail.com>
2018-07-09 16:59:17 -0700

    final commit 1

--- src/components/WizardFour/WizardFour.js
+++ src/components/WizardFour/WizardFour.js
@@ -1,5 +1,7 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {updateFound} from '../../ducks/reducer';
+import {connect} from 'react-redux';
 
 class WizardFour extends Component {
     render(){
@@ -9,13 +11,19 @@ class WizardFour extends Component {
                     <p>Have you already found your new home?</p> <br />
                     
                     <div className="row">
-                        <Link to="/wFive"><button onClick={this.props.foundTrue}>Yes</button></Link>
-                        <Link to="/wFive"><button onClick={this.props.foundFalse}>No </button></Link>  
+                        <Link to="/wFive"><button onClick={() => updateFound(true)}>Yes</button></Link>
+                        <Link to="/wFive"><button onClick={() => updateFound(false)}>No </button></Link>  
                     </div>         
                 </div>
             </div>
         )
     }
 }
+function mapStateToProps(state){
+    const {found} = state;
+    return {
+        found,
+    }
+}
 
-export default WizardFour;
+export default connect(mapStateToProps, {updateFound})(WizardFour);
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -20,6 +20,7 @@ const UPDATE_LOAN_TYPE = 'UPDATE_LOAN_TYPE';
 const UPDATE_PROPERTY_TYPE = 'UPDATE_PROPERTY_TYPE';
 const UPDATE_CITY = 'UPDATE_CITY';
 const UPDATE_PROP = 'UPDATE_PROP';
+const UPDATE_FOUND = 'UPDATE_FOUND';
 
 function reducer(state = initialState, action){
 if (action.type === UPDATE_LOAN_TYPE){
@@ -30,9 +31,92 @@ if (action.type === UPDATE_LOAN_TYPE){
     return {...state, city: action.payload}
 } else if (action.type === UPDATE_PROP){
     return {...state, propToBeUsedOn: action.payload}
-} return state;
+} else if(action.type === UPDATE_FOUND){
+    return {...state, found: action.payload}
+} else if(action.type === 'UPDATE_REALESTATEAGENT'){
+    return {...state, agent: action.payload}
+} else if(action.type === 'UPDATE_COST'){
+    return {...state, cost: action.payload}
+} else if (action.type === 'UPDATE_DOWNPAYMENT'){
+    return {...state, downPayment: action.payload}
+} else if (action.type === 'UPDATE_CREDIT'){
+    return {...state, credit: action.payload}
+} else if (action.type === 'UPDATE_HISTORY'){
+    return {...state, history: action.payload}
+} else if (action.type === 'UPDATE_ADDRESSONE'){
+    return {...state, addressOne: action.payload}
+} else if (action.type === 'UPDATE_ADDRESSTWO'){
+    return {...state, addressOne: action.payload}
+} else if (action.type === 'UPDATE_ADDRESSTHREE'){
+    return {...state, addressOne: action.payload}
+} else if (action.type === 'UPDATE_FIRSTNAME'){
+    return {...state, firstName: action.payload}
+} else if (action.type === 'UPDATE_LASTNAME'){
+    return {...state, lastName: action.payload}
+} else if (action.type === 'UPDATE_EMAIL'){
+    return {...state, email: action.payload}
+}return state;
 }
-
+export function updateEmail(email){
+    return {
+        type: 'UPDATE_EMAIL',
+        payload: email,
+    }
+}
+export function updateFirstName(firstName){
+    return {
+        type: 'UPDATE_FIRSTNAME',
+        payload: firstName,
+    }
+}
+export function updateLastName(lastName){
+    return {
+        type: 'UPDATE_LASTNAME',
+        payload: lastName,
+    }
+}
+export function updateAddressOne (addressOne){
+    return {
+        type: 'UPDATE_ADDRESSONE',
+        payload: addressOne,
+    }
+}
+export function updateAddressTwo (addressTwo){
+    return {
+        type: 'UPDATE_ADDRESSTWO',
+        payload: addressTwo,
+    }
+}
+export function updateAddressThree (addressThree){
+    return {
+        type: 'UPDATE_ADDRESSTHREE',
+        payload: addressThree,
+    }
+}
+export function updateHistory (history){
+    return {
+        type: 'UPDATE_HISTORY',
+        payload: history,
+    }
+}
+export function updateCredit(credit){
+    return {
+        type: 'UPDATE_CREDIT',
+        payload: credit,
+    }
+}
+export function updateDownPayment(payment){
+    return {
+        type: 'UDPATE_DOWNPAYMENT',
+        payload: payment,
+    }
+}
+export function updateCost(cost){
+    return {
+        type: 'UPDATE_COST',
+        payload: cost,
+    }
+}
 export function updateLoanType(loantype){
     return{
     type: UPDATE_LOAN_TYPE,
@@ -55,6 +139,18 @@ export function updateProp(prop){
     return {
         type: UPDATE_PROP,
         payload: prop,
+    }
+}
+export function updateFound(found){
+    return {
+        type: UPDATE_FOUND,
+        payload: found,
+    }
+}
+export function updateRealEstateAgent(agent){
+    return {
+        type: 'UPDATE_AGENT',
+        payload: agent,
     }
 }
 export default reducer;
